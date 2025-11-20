ace7153a4b7d5e3bb876a2f00672d996f82335dd
aboutKGT <kgt1097@gmail.com>
2025-11-20 22:37:22 +0000

    Update settings.json and default.py

--- dj/script/default.py
+++ dj/script/default.py
@@ -2,5 +2,8 @@ import detectron2
 from detectron2.config import get_cfg
 from detectron2.engine import DefaultTrainer
 
-print("Detectron2 version:", detectron2.__version__)
+try:
+    print("Detectron2 version:", detectron2.__version__)
+except AttributeError:
+    print("Detectron2 imported successfully")
 
